942708560219706ef6da67ab94b449dd69945627
Muslim Sidiq <muslimsidiq2@gmail.com>
2021-06-07 10:58:02 +0700

    Add param

--- index.php
+++ index.php
@@ -1,24 +1,39 @@
 <?php
-	function convertCurrencyXE($amount, $from, $to){
-		$data = file_get_contents("https://www.xe.com/currencyconverter/convert/?Amount=$amount&From=$from&To=$to");
-		//var_dump($data);
-		$doc = new DOMDocument;
-		libxml_use_internal_errors(true);
-		$doc->loadHTML($data);
-		libxml_use_internal_errors(false);
+require_once("rest.php");
 
-		$xpath = new DOMXPath($doc);
-		$result = $xpath->query('//p[starts-with(@class,"result__BigRate")]');
-		if($result->length > 0) {
-		  $node = $result->item(0);
-		  $value = $node->nodeValue;
-		  return explode(" ", $value)[0];
-		} else {
-		  return -1;
-		}
+function convertCurrencyXE($amount, $from, $to){
+	$data = file_get_contents("https://www.xe.com/currencyconverter/convert/?Amount=$amount&From=$from&To=$to");
+	//var_dump($data);
+	$doc = new DOMDocument;
+	libxml_use_internal_errors(true);
+	$doc->loadHTML($data);
+	libxml_use_internal_errors(false);
+
+	$xpath = new DOMXPath($doc);
+	$result = $xpath->query('//p[starts-with(@class,"result__BigRate")]');
+	if($result->length > 0) {
+	  $node = $result->item(0);
+	  $value = $node->nodeValue;
+	  return explode(" ", $value)[0];
+	} else {
+	  return -1;
 	}
+}
 
+$rest = new REST();
 
-echo convertCurrencyXE("1", "USD", "IDR");
+if($rest->get_request_method() != "GET") $rest->response('',406);
+if(!isset($rest->_request['from'])) $rest->responseInvalidParam('from');
+if(!isset($rest->_request['to'])) $rest->responseInvalidParam('to');
+$from = $rest->_request['from'];
+$to = $rest->_request['to'];
+$amount = 1;
+if(isset($rest->_request['amount'])){
+    $amount = (int)$rest->_request['amount'];
+}
+
+$result = convertCurrencyXE($amount, $from, $to);
+
+$rest->show_response(array('result' => $result));
 
 ?>
